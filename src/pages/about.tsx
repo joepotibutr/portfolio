c99a4d90ddd4d4e84993a81406f50a4586d5814d
vchrpng <vchrpng@gmail.com>
2019-08-23 19:12:19 +0700

    update navigation scrolling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,14 +13,16 @@ export default class About extends Component<Props, State> {
     render() {
         return (
            
-
-    <section>
+<div>
+<section>
           <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vestibulum gravida mi id consequat. Nunc sodales auctor libero, tincidunt feugiat tortor pretium sit amet. Mauris pretium nisl nec lectus sodales, nec aliquet ligula iaculis. Ut euismod nulla libero, vel ullamcorper velit faucibus in. Fusce scelerisque congue odio, ullamcorper mollis felis pharetra eu. Ut aliquam massa eget lectus elementum, eget mollis urna pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a porttitor augue. Nunc et nulla fringilla, eleifend lacus in, egestas leo. Suspendisse potenti. Morbi ut erat eu libero lacinia pretium. In tincidunt massa eget erat porttitor pellentesque. Integer libero lacus, venenatis nec nisl et, luctus molestie diam. Quisque accumsan ligula id enim eleifend, sed blandit velit pretium. Sed pretium, mi non congue ullamcorper, nisi mi scelerisque nisl, sit amet malesuada neque erat non mi.
     </p>
     </section>
+</div>
+
         )
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,35 +10,25 @@ import { NAVIGATION } from "../constants";
 import Projects from "./projects";
 
 
-const aboutRef: React.RefObject<About> = React.createRef()
-const expRef: React.RefObject<Experience> = React.createRef()
-const projectsRef: React.RefObject<Projects> = React.createRef()
-const skillsRef : React.RefObject<Skills>= React.createRef()
+const aboutRef: React.RefObject<HTMLDivElement> = React.createRef()
+const expRef: React.RefObject<HTMLDivElement> = React.createRef()
+const projectsRef: React.RefObject<HTMLDivElement> = React.createRef()
+const skillsRef : React.RefObject<HTMLDivElement>= React.createRef()
 
 const gotoNavigation = (destination: NAVIGATION) => {
-  let ref: React.RefObject<any> | null = null
   switch(destination) {
     case NAVIGATION.ABOUT :
-      alert('yey')
-      ref = aboutRef
-      break
+      return aboutRef.current && window.scrollTo(0, aboutRef.current.offsetTop)
     case NAVIGATION.SKILLS :
-      ref = skillsRef
-      break
+      return skillsRef.current && window.scrollTo(0, skillsRef.current.offsetTop)
     case NAVIGATION.EXPERIENCE : 
-      ref = expRef
-      break
+      return expRef.current && window.scrollTo(0, expRef.current.offsetTop)
     case NAVIGATION.PROJECTS : 
-      ref = projectsRef
-      break
+      return projectsRef.current && window.scrollTo(0, projectsRef.current.offsetTop)
   }
-
-  return ref && ref.current && window.scrollTo(0, ref.current.offsetTop)
 }
 
 export const NavigationContext = React.createContext(gotoNavigation)
-
-
 
 
 const IndexPage = () => {
@@ -47,10 +37,18 @@ const IndexPage = () => {
     <NavigationContext.Provider value={gotoNavigation} >
       <Layout>
         <SEO title="Home" />
-        <About ref={aboutRef} />
-        <Experience ref={expRef} />
-        <Projects ref={projectsRef} />
-        <Skills ref={skillsRef} />
+        <div ref={aboutRef}>
+          <About  />
+        </div>
+        <div ref={expRef}>
+          <Experience  />
+        </div>
+        <div ref={projectsRef}>
+        <Projects  />
+        </div>
+        <div ref={skillsRef} > 
+        <Skills />
+        </div>
       </Layout>
     </NavigationContext.Provider>
    
