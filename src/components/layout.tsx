c768f2460847bc7971f3b48876884e773ef8997f
vchrpng <vchrpng@gmail.com>
2019-09-02 20:09:12 +0700

    add more spacing for layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,9 +28,9 @@ const Layout = ({ children }: any) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
+          padding: '20px',
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
+          maxWidth: 720,
           paddingTop: 0,
         }}
       >
