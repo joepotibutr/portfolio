cd5d4c739d91fe7933564a8da40f8ddd3a9ba927
vchrpng <vchrpng@gmail.com>
2019-09-07 18:40:29 +0700

    add section component

---
+++ src/components/section.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface Props {
+    height: number;
+    children?: React.ReactChild | React.ReactNode;
+}
+
+interface SectionStyleProps {
+    height: number
+}
+
+const SectionStyled = styled.section<SectionStyleProps>`
+    display: flex;
+    justify-content: center;
+    align-items: center;        
+    flex-direction: column;
+    height: ${(props) => props.height}vh; 
+    margin-top: 100px;
+`
+
+export default function Section({ children, height }: Props) {
+    return (
+        <SectionStyled height={height}>
+            {children}
+        </SectionStyled>
+    )
+}
