db2f557188bee2daf55bbaad68cb58793b7d5b98
vchrpng <vchrpng@gmail.com>
2019-09-07 18:40:10 +0700

    separate and refactor header style properly

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,64 +4,64 @@ import React from "react"
 import { NAVIGATION } from "../constants";
 
 import {NavigationContext} from '../pages/index'
+import styled from 'styled-components'
+
+const HeaderStyled = styled.header`
+  top: 0;
+  position: fixed;
+  background: white;
+  height: 80px;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`
+
+const HeaderOuterWrapper = styled.div`
+  max-width: 720;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
 
 
 const Header = ({ siteTitle }: any) => (
-  <header
-    style={{ 
-      top: 0,
-      position: 'fixed',
-      background: 'white',
-      height: '80px',
-      padding: '20px',
-      display: 'flex',
-      justifyContent: 'center',
-      width: '100%'
-    }}
-  >
-    <div style={{
-          maxWidth: 720,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          width: '100%'
-    }}>
-    <div
-    
-    >
-      <h1 style={{ 
-        fontSize: '1em',
-        margin: 0,
-        textDecoration: `none`, }}>
+  <HeaderStyled>
+    <HeaderOuterWrapper>
+      <div>
+        <h1 style={{ 
+          fontSize: '1em',
+          margin: 0,
+          textDecoration: `none`, }}>
 
-        <span>{siteTitle} -</span>
-        <span style={{ fontWeight: 'lighter' }}> Full Stack Engineer</span>
-      </h1>
-    </div>
-    <NavigationContext.Consumer>
-    {(goto) => (
-    <nav style={{ 
-      display: 'flex',
-      fontSize: '1em'
-       }}>
-         {Object.values(NAVIGATION).map(section => (
-          <div onClick={() => goto(section)}>
-            <h1 style={{ 
-              fontSize: '1em',
-              margin: 0,
-              textDecoration: `none`,
-              marginRight: '10px'
-            }}
-              >
-              {section.charAt(0) + section.slice(1).toLowerCase()}
-            </h1>
-          </div>
-         ))}
-    </nav>)}
-    </NavigationContext.Consumer>
-    </div>
-   
-  </header>
+          <span>{siteTitle} -</span>
+          <span style={{ fontWeight: 'lighter' }}> Full Stack Engineer</span>
+        </h1>
+      </div>
+      <NavigationContext.Consumer>
+      {(goto) => (
+      <nav style={{ 
+        display: 'flex',
+        fontSize: '1em'
+        }}>
+          {Object.values(NAVIGATION).map(currentSection => (
+            <div onClick={() => goto(currentSection)}>
+              <h1 style={{ 
+                fontSize: '1em',
+                margin: 0,
+                textDecoration: `none`,
+                marginRight: '10px'
+              }}
+                >
+                {currentSection.charAt(0) + currentSection.slice(1).toLowerCase()}
+              </h1>
+            </div>
+          ))}
+      </nav>)}
+      </NavigationContext.Consumer>
+    </HeaderOuterWrapper>
+  </HeaderStyled>
 )
 
 Header.propTypes = {
