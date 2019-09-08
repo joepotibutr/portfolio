0f02670f682500f66125c396d9a966cc07257726
vchrpng <vchrpng@gmail.com>
2019-09-08 23:29:23 +0700

    update reusable component

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Section } from '../components'
+import { Section, Button } from '../components'
+import styled from 'styled-components'
 
 interface Props {
     
@@ -8,28 +9,43 @@ interface State {
     
 }
 
+const FormInput = styled.div`
+    margin-bottom:  10px;
+`
+
+const TextInput = styled.input`
+    border-style: none;
+    border: 1px solid;
+    display: block;
+    width: 100%;
+    border-radius: 4px;
+    padding: 5px;
+    &:focus {
+        outline: none;
+    }
+`
+
 export default class Contact extends Component<Props, State> {
     state = {}
 
     render() {
         return (
             <Section height={70}>
+                <div style={{ width : '100%', height: '100%'}}>
                 <h2>Contact</h2>
                 <form action="">
-                    <input placeholder="Full name*" style={{ display: 'block' }} type="text"/>
-                    <input placeholder="Email*" style={{ display: 'block' }} type="email"/>
-                    <textarea style={{ display: 'block' }} placeholder="Message*" name="" id="" cols={30} rows={10}></textarea>
-                    <button style={{
-                            cursor:'pointer',
-                            borderRadius: '4px',
-                            fontWeight: 'bold',
-                            fontSize: '14px',
-                            height: '50px',
-                            background: 'black',
-                            color: 'white',
-                            width: '150px',
-                    }} type="submit">Submit</button>
+                    <FormInput>
+                        <TextInput placeholder="Full name*" type="text"/>
+                    </FormInput>
+                    <FormInput>
+                        <TextInput placeholder="Email*" type="email"/>
+                    </FormInput>
+                    <FormInput>
+                        <textarea style={{ padding: '5px' ,display: 'block', width: '100%',borderRadius: '4px' }} placeholder="Message*" name="" id="" cols={30} rows={10}></textarea>
+                    </FormInput>
+                    <Button type="submit">Submit</Button>
                 </form>
+                </div>
             </Section>
         )
     }
