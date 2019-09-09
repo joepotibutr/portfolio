a52595f0dbf6276c435196983e88bbaa74d2295e
vchrpng <vchrpng@gmail.com>
2019-09-10 00:28:19 +0700

    styling project box

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -14,18 +14,30 @@ interface State {
 const ProjectsList = styled.ul`
     display:grid;
     grid-template-columns: 1fr 1fr;
+    grid-template-rows: 250px;
     grid-gap: 10px;
     margin:0;
 
 `
 
+const Tag = styled.span`
+    background: lightgray;
+    border-radius: 3px;
+    font-size: 12px;
+    padding: 5px;
+    margin-right: 10px;
+`
+
 const ProjectItemBox = styled.li`
     list-style: none;
-    background: gray;
     padding: 20px;
     border-radius: 4px;
     cursor: pointer;
     position: relative;
+    box-shadow: gray 1px 1px 3px 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
     &:after {
         width: 100%;
@@ -37,7 +49,7 @@ const ProjectItemBox = styled.li`
         content: '';
         transition: opacity .3s;
         opacity: 0;
-        box-shadow:black 1px 1px 12px 0px;
+        box-shadow:gray 1px 1px 12px 0px;
         border-radius: 4px;
     }
 
@@ -61,11 +73,19 @@ export default class Projects extends Component<Props, State> {
                     <ProjectsList>
                     {projects.map((project: any) => (
                         <ProjectItemBox>
-                        <h2>{project.name}</h2> <span>{project.year}</span>
-                        <p>{project.description}</p>
-                        {project.techstack.map((tech: string) => (
-                            <span>{tech}</span>
+                            <div>
+                                <h4 style={{
+                                display: 'flex',
+                                justifyContent: 'space-between'
+                            }}>{project.name}<span>{project.year}</span></h4> 
+                            <p>{project.description}</p>
+                            </div>
+                            <div>
+                            {project.techstack.map((tech: string) => (
+                            <Tag>#{tech}</Tag>
                         ))}
+                            </div>
+                       
                         </ProjectItemBox>
                     ))}
                     </ProjectsList>
