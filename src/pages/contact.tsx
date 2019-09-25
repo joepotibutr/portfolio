fc5cb7d97743e463c4437aac6b97e375baf861b0
vchrpng <vchrpng@gmail.com>
2019-09-25 16:14:10 +0530

    be able to sent message to me via form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Section, Button } from '../components'
 import styled from 'styled-components'
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const FormInput = styled.div`
     margin-bottom:  10px;
@@ -19,27 +23,41 @@ const TextInput = styled.input`
 `
 
 export default () => {
-    const [fullName,setFullname] = React.useState('')
-    const [email,setEmail] = React.useState('')
-    const [message,setMessage] = React.useState('')
+    const [fullName, setFullname] = React.useState('')
+    const [emailAddress, setEmail] = React.useState('')
+    const [message, setMessage] = React.useState('')
+    const [loading, setLoading] = React.useState(false)
+
+    const sendContactMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+            await axios.post(process.env.DATAFIRE!,{
+                message,
+                emailAddress
+            })
+            alert('successful')
+        } catch {
+            alert('!!!')
+        }
+        setLoading(false)
+    }
+
     return (
         <Section height={70}>
             <div style={{ width : '100%', height: '100%'}}>
             <h2>Contact</h2>
-            <form onSubmit={e => {e.preventDefault() 
-            console.log({
-                fullName, email, message
-            })}} action="">
+            <form onSubmit={sendContactMessage} action="">
                 <FormInput>
                     <TextInput value={fullName} onChange={(e) => setFullname(e.currentTarget.value)} required placeholder="Full name*" type="text"/>
                 </FormInput>
                 <FormInput>
-                    <TextInput value={email} onChange={(e) => setEmail(e.currentTarget.value)} required placeholder="Email*" type="email"/>
+                    <TextInput value={emailAddress} onChange={(e) => setEmail(e.currentTarget.value)} required placeholder="Email*" type="email"/>
                 </FormInput>
                 <FormInput>
                     <textarea value={message} onChange={(e) => setMessage(e.currentTarget.value)} required style={{ padding: '5px' ,display: 'block', width: '100%',borderRadius: '4px' }} placeholder="Message*" name="" id="" cols={30} rows={10}></textarea>
                 </FormInput>
-                <Button type="submit">Submit</Button>
+                {loading ? <h1>Loading</h1> : <Button type="submit">Submit</Button>}
             </form>
             </div>
         </Section>
