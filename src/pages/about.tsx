a6e05b2b7600bf7eac1e73f1ce215cca08a151ff
vchrpng <vchrpng@gmail.com>
2019-10-04 15:23:41 +0700

    update responsive breakpoints

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,12 +8,41 @@ const  IndividualPicture = require('../assets/individual.png')
 
 const AboutPage = styled.div`
     margin-bottom: 20px;
-    display: flex;
+    display: grid;
+    grid-template-columns: 50% 50%;
     height: 100%;
-    @media only screen and (max-width: 480px) {
-        flex-direction: column;
-        > div {
-            width: 100% !important;
+    width:100%;
+
+    .welcome {
+        grid-column:1/2;
+        grid-row:1/2;
+    }
+
+    .self-cartoon {
+        grid-column:2/3;
+        grid-row:1/3;
+    }
+
+    .tour-button {
+        grid-column:1/2;
+        grid-row: 2/3;
+    }
+
+
+    @media only screen and (max-width: 720px) {
+        grid-template-columns: auto;
+        grid-template-rows: min-content;
+        .welcome {
+            text-align:center;
+        }
+        
+        .self-cartoon {
+            grid-column:1/2;
+            grid-row:2/3;
+        }
+        .tour-button {
+            grid-column:1/2;
+            grid-row: 3/4;
         }
     }
 `
@@ -22,31 +51,23 @@ export default () => {
     return (
         <Section height={60}>
             <AboutPage>
-                <div style={{
-                    height: '100%',
-                    width: '50%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                }}>
-                    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'  }}>
-                        <h1>Hi people</h1>
-                        <h2 style={{ color: 'darkgray' }}>I'm Joe and I'm a full stack web developer </h2>
+                <div className="welcome" style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'  }}>
+                    <h1>Hi people</h1>
+                    <h2 style={{ color: 'darkgray' }}>I'm Joe and I'm a full stack web developer </h2>
+                </div>
+                <div className="tour-button" style={{ width: '100%', display: 'flex',justifyContent: 'center' }}>
+                    <div style={{ marginRight: '20px' }}>
+                        <Button onClick={() => window.open('https://drive.google.com/file/d/1oIE0cHlGoacFtfgjx7RjfyuP0G8SksK1/view?usp=sharing')}>View Resume</Button>
                     </div>
-                    <div style={{ width: '100%', display: 'flex' }}>
-                        <div style={{ marginRight: '20px' }}>
-                            <Button onClick={() => window.open('https://drive.google.com/file/d/1oIE0cHlGoacFtfgjx7RjfyuP0G8SksK1/view?usp=sharing')}>View Resume</Button>
-                        </div>
-                        <div>
-                            <NavigationContext.Consumer>
-                            {(goto) => (
-                                <Button onClick={() => goto(NAVIGATION.CONTACT)}>Get in touch</Button>
-                            )}
-                            </NavigationContext.Consumer>
-                        </div>
+                    <div>
+                        <NavigationContext.Consumer>
+                        {(goto) => (
+                            <Button onClick={() => goto(NAVIGATION.CONTACT)}>Get in touch</Button>
+                        )}
+                        </NavigationContext.Consumer>
                     </div>
                 </div>
-                <div style={{ width: '50%', display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                <div className="self-cartoon" style={{ display: 'flex', alignItems: 'center',justifyContent:'center' }}>
                     <img style={{ maxWidth: '350px', width: '100%', height: 'auto' }} src={IndividualPicture} />
                 </div>
             </AboutPage>
