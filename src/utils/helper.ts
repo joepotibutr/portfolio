f2c9506ebe66059c26a39cc4ba1064d8ce2051ae
vchrpng <vchrpng@gmail.com>
2019-10-04 15:44:05 +0700

    update types

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, MutableRefObject } from "react";
 
 
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 
-export const useOutsideClick = (ref: any, callback: any) => {
+export const useOutsideClick = (ref: MutableRefObject<null | HTMLDivElement>, callback: () => void) => {
   const handleClick = (e: any) => {
     if (ref.current && !ref.current.contains(e.target)) {
       callback();
