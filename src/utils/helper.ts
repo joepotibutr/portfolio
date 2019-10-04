e8661b0d993471ab40e124e2bfc54a044b0940e8
vchrpng <vchrpng@gmail.com>
2019-10-04 15:39:27 +0700

    create ouside click helper function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,1 +1,21 @@
+import { useEffect } from "react";
+
+
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+
+export const useOutsideClick = (ref: any, callback: any) => {
+  const handleClick = (e: any) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
+};
