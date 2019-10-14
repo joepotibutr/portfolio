696537f354d69c6c2864ec0713018187f1754185
vchrpng <vchrpng@gmail.com>
2019-10-14 18:53:28 +0700

    update props for button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ButtonStyled = styled.button<{ fluid?: boolean }>`
+const ButtonStyled = styled.button<{ fluid?: boolean, transparent?: boolean }>`
     cursor:pointer;
     border-radius: 4px;
     font-weight:bold;
     font-size: 14px;
-    height: ${props => props.fluid ? '100%' : '50px'};
+    height: ${props => props.fluid ? '100%' : '45px'};
     background: #1C5F9C;
     color: white;
     width: ${props => props.fluid ? '100%' : '150px'};
@@ -20,18 +20,24 @@ const ButtonStyled = styled.button<{ fluid?: boolean }>`
     &:hover {
         background:#0e2f4e;
     }
+
+    ${props => props.transparent && `
+        background: transparent;
+        color: black;
+    `}
 `
 
 interface IButtonProps {
-    children: React.ReactChild,
+    children: React.ReactNode
     onClick?: () => void,
     type?: 'submit',
     fluid?: boolean 
+    transparent?: boolean
 }
 
-const Button = ({ children, onClick, type, fluid }: IButtonProps) => {
+const Button = ({ children, onClick, type, fluid, transparent }: IButtonProps) => {
     return (
-        <ButtonStyled fluid={fluid} type={type} onClick={onClick}>
+        <ButtonStyled transparent={transparent} fluid={fluid} type={type} onClick={onClick}>
             {children}
         </ButtonStyled>
     )
