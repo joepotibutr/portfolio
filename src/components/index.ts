6e38ef144a59c62254cd41e0fd6a4f79b9440529
vchrpng <vchrpng@gmail.com>
2019-10-17 19:51:20 +0700

    add modal component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Section from './section'
 import SlideUp from './slideup'
 import FormInput from './form-input'
 import Wave from './wave'
+import Modal from './modal'
 
-export { Button, Section, SlideUp, FormInput, Wave }
+export { Button, Section, SlideUp, FormInput, Wave, Modal }
