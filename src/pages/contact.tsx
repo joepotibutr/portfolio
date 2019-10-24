70017ad0d3c9190c58ae3a75bc03ac5e4aec364c
vchrpng <vchrpng@gmail.com>
2019-10-24 18:48:35 +0700

    logging

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -106,8 +106,10 @@ export default () => {
         setLoading(true)
         sleep(2000).then(() => {
             try {
+                console.log(process.env.DATAFIRE)
                 axios.post(process.env.DATAFIRE!, values)
             } catch {
+                console.log('err')
             }
             setMessageSent(true)
             setLoading(false)
